f039cf0fac971f2c2fc368dcea0b4dc458bad55e
ZacharyMayle <zam1998@gmail.com>
2020-02-04 11:53:44 -0700

    Added class relationships to planet and modifer

--- app/models/modifier.rb
+++ app/models/modifier.rb
@@ -1,2 +1,4 @@
 class Modifier < ApplicationRecord
+    has_many :games
+    has many :modifiers, through: :games
 end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,4 @@
 class Planet < ApplicationRecord
+    has_many :games
+    has_many :planets, through: :games
 end
